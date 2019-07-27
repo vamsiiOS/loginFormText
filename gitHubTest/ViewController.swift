76666c203d8e155d32436e82980893d3a75ceb98
vamsi makaraju <vamsi.makaraju@achiever-40.local>
2019-07-27 11:39:56 +0530

    Declared TF Names

--- gitHubTest/ViewController.swift
+++ gitHubTest/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UIStackView!
+    @IBOutlet weak var lastName: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //jkuhjhjkhkjh
         /*jkljkljkljljljlkjklj*/
+        
+        
+        
     }
 
 
